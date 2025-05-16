a9c9bf715f566e81efa6630e2d0579d617275e09
steeven-janny <steeven.janny@naverlabs.com>
2025-05-16 12:05:34 +0200

    Up

--- js/arxiv_api.js
+++ js/arxiv_api.js
@@ -1,66 +1,50 @@
 function string_sanitizer(str) {
-    // Remove special characters, convert to lowercase, replace spaces with +
-    // and remove trailing +
-    return str.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase().replace(/ /g, "+").replace(/\+$/, "");
+    let formated_str = str.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase()
+    formated_str = formated_str.replace(/\+$/, "")
+    return formated_str.split(" ")
+
 }
 
-function forge_query(keyword){
+function forge_query(keyword) {
     // Check value of radio buttons
     var radio_buttons = document.getElementsByName("search_type");
     var query = "https://export.arxiv.org/api/query?search_query=";
-    query +=  "(cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CV+OR+cat:cs.NE)"
+    query += "(cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CV+OR+cat:cs.NE)"
 
-    if (radio_buttons[2].checked) {
-        query +=  "+AND+"+string_sanitizer(keyword);
-    }
-    else if (radio_buttons[0].checked) {
-        query += "+AND+ti:" + string_sanitizer(keyword);
-    }
-    else if (radio_buttons[1].checked) {
-        query += "+AND+abs:" + string_sanitizer(keyword);
-    }
+    let clean_keywords = string_sanitizer(keyword)
+    const search_prefix = radio_buttons[2].checked ? "" : radio_buttons[0].checked ? "ti:" : "abs:"
+    clean_keywords.forEach(element => {
+        if (element !== "") {
+            query += "+AND+" + search_prefix + element;
+        }
+    })
     query += "&start=0&max_results=10000&sortBy=lastUpdatedDate&sortOrder=descending";
     return query;
 
 }
+
 function arxiv_search(keyword) {
-    // var cat = "(cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CV+OR+cat:cs.NE)"
-    // var url = "http://export.arxiv.org/api/query?search_query=" + cat + "+AND+ti:" + string_sanitizer(keyword) + "&start=0&max_results=10000&sortBy=lastUpdatedDate&sortOrder=descending";
-    var url = forge_query(keyword);
-    var xhr = new XMLHttpRequest();
+    const url = forge_query(keyword);
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             document.getElementById("graph_title").innerHTML = "Results for \"" + keyword + "\"";
 
             loading_anim.innerHTML = ""
-            var results = filterResults(xhr.responseXML);
+            let results = filterResults(xhr.responseXML);
 
             // Sort the results by date (ascending)
             results.sort(function (a, b) {
                 return new Date(a.date) - new Date(b.date);
             });
 
-            // Compute cumulative sum of the number of papers
-            var cumsum = [];
-            var sum = 0;
-            for (var i = 0; i < results.length; i++) {
-                sum += 1;
-                cumsum.push({
-                    "date": new Date(results[i].date),
-                    "value": sum
-                });
-            }
-
-            updateChart1(cumsum);
-
             var authors = paper_per_author(results);
-            updateChart2(authors);
+            updateAuthorBlocks(authors);
             if (results.length > 0) {
                 loading_anim.innerHTML = "";
-            }
-            else {
-                loading_anim.innerHTML = "<div class=\"alert alert-danger\" role=\"alert\">No results found (maybe check the caveat ?)</div>";
+            } else {
+                loading_anim.innerHTML = "<div class=\"alert alert-danger\" role=\"alert\">No results found...</div>";
             }
 
         }
@@ -92,6 +76,7 @@ function filterResults(response) {
     return filteredResults;
 }
 
+
 function paper_per_author(results) {
     var authors = {};
     for (var i = 0; i < results.length; i++) {
@@ -107,20 +92,8 @@ function paper_per_author(results) {
     }
     return authors;
 }
-
-// check GET parameters
-var urlParams = new URLSearchParams(window.location.search);
-var keyword = urlParams.get('keyword');
-if (keyword) {
-    document.getElementById("keywords_field").value = keyword;
-
-    loading_anim.innerHTML = "<div class=\"spinner-border\" role=\"status\">";
-    arxiv_search(keyword);
-
-}
-
-document.getElementById('myForm').onsubmit = function () {
-    testResults(this);
+document.getElementById('search_form').onsubmit = function () {
+    launchSearch(this);
     return false;
 }
 
@@ -131,9 +104,8 @@ document.getElementById('myForm2').onsubmit = function () {
 var loading_anim = document.getElementById('loading_anim');
 
 
-function testResults(form) {
-    var inputValue = form.keyword.value;
-    loading_anim.innerHTML = "<div class=\"spinner-border\" role=\"status\">";
-    arxiv_search(inputValue);
+function launchSearch(form) {
+    loading_anim.innerHTML = "<div class=\"spinner-border\" role=\"status\"></div><span>This might take a while...</span>";
+    arxiv_search(form.keyword.value);
 }
 
